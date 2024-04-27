06bdcafecf46402b6a1d082ee023b699e479a1b4
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-27 18:43:20 +0530

    insert data into user with sql2 packages done

--- NodeWithSql/index.js
+++ NodeWithSql/index.js
@@ -5,18 +5,26 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "instagram",
+  database: "college",
   password: "Tanmoy@1993",
 });
 
-let q = "SHOW TABLES"
+// let q = "INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)";
+let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let user = ["123","user_1","abc@gmail.com","pass1"]
+let user = [
+  ["124", "user_2", "abcd@gmail.com", "pass2"],
+  ["125", "user_3", "abcde@gmail.com", "pass3"],
+  ["126", "user_4", "abcdef@gmail.com", "pass4"],
+];
 try {
-  connection.query(q, (err, results) => {
+  // connection.query(q, user, (err, results) => {
+  connection.query(q, [user], (err, results) => {
     if (err) throw err;
     console.log(results); // results contains rows returned by server
-    console.log(results.length); // results contains rows returned by server
-    console.log(results[0]); // results contains rows returned by server
-    console.log(results[1]); // results contains rows returned by server
+    // console.log(results.length); // results contains rows returned by server
+    // console.log(results[0]); // results contains rows returned by server
+    // console.log(results[1]); // results contains rows returned by server
   });
 } catch (err) {
   console.log(err);
