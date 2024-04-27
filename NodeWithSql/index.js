509c5f097f15420d46462e0fe8ff846283fc4102
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-27 19:40:26 +0530

    adding ejs template done

--- NodeWithSql/index.js
+++ NodeWithSql/index.js
@@ -1,39 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
 // Create the connection to database
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "college",
-    password: "Tanmoy@1993",
-  });
+  host: "localhost",
+  user: "root",
+  database: "college",
+  password: "Tanmoy@1993",
+});
 
-  const getRandomUser = () => {
-    return [
-      faker.string.uuid(),
-      faker.internet.userName(),
-      faker.internet.email(),
-      faker.internet.password(),
-    ];
-  };
-app.get("/",(req,res)=>{
-let q = "SELECT count(*) FROM user"
-try {
-    connection.query(q, (err,result)=>{
-        if (err) throw err;
-        console.log(result[0]["count(*)"]);
-        res.send(result[0]);
-    })
-} catch (error) {
+const getRandomUser = () => {
+  return [
+    faker.string.uuid(),
+    faker.internet.userName(),
+    faker.internet.email(),
+    faker.internet.password(),
+  ];
+};
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) FROM users";
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let count = result[0]["count(*)"];
+      res.render("home.ejs", { count });
+    });
+  } catch (error) {
     console.log(error);
-    res.send("Some error occurred in DB")
-}
-})
+    res.render("error.ejs");
+  }
+});
 
-app.listen(port , ()=>{
-console.log("Server is listening on port " + port);
-})
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
